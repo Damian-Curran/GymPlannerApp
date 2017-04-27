cfb61a3b3fccc4c8a7148a95f033b760c845f78d
Damian404 <sgt,curren@gmail.com>
2017-04-27 01:33:27 +0100

    Edited MuscleGroup page/ Added drop down menus

--- GymPlanner/PlanExercises.xaml.cs
+++ GymPlanner/PlanExercises.xaml.cs
@@ -20,30 +20,11 @@ namespace GymPlanner
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MuscleGroups : Page
+    public sealed partial class PlanExercises : Page
     {
-        public MuscleGroups()
+        public PlanExercises()
         {
             this.InitializeComponent();
-            ComboBox();
-        }
-
-        private void ComboBox()
-        {
-            string[] employees = new string[]{"Biceps", "Triceps",
-            "Legs", "Shoulders", "Chest",
-            "Abs", "Back"};
-
-            for (int i = 0; i < 7; i++)
-            {
-                Exercise1.Items.Add(employees[i]);
-                Exercise2.Items.Add(employees[i]);
-            }
-        }
-
-        private void btn5_click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(PlanExercises));
         }
     }
 }
