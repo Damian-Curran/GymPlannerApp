3e102f6eaea0397c8fdc3292de4077665d4f319c
Damian404 <sgt,curren@gmail.com>
2017-04-27 03:38:27 +0100

    Created method in PlanExcersises to create grid for selected muscle excercises

--- GymPlanner/PlanExercises.xaml.cs
+++ GymPlanner/PlanExercises.xaml.cs
@@ -47,7 +47,7 @@ namespace GymPlanner
             }
             else if (obj.pagePath == 2)
             {
-                
+                CreateGridSelected();
             }
             else if (obj.pagePath == 1)
             {
@@ -147,7 +147,6 @@ namespace GymPlanner
                         myBorder3.BorderThickness = new Thickness(1);
 
                         TextBlock label3 = new TextBlock();
-                        //label3.Text = reee[i, 4];
                         label3.FontSize = 10;
                         label3.VerticalAlignment = VerticalAlignment.Bottom;
 
@@ -159,6 +158,127 @@ namespace GymPlanner
                         Grid.SetColumn(myBorder3, 1);
                         Grid.SetRow(myBorder3, counter);
                         DynamicGrid.Children.Add(myBorder3);
+                    }
+                }
+            }
+
+            ExerciseLayout.Children.Add(DynamicGrid);
+        }
+
+        private void CreateGridSelected()
+        {
+            Grid DynamicGrid = new Grid();
+            DynamicGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
+            DynamicGrid.Background = new SolidColorBrush(Colors.LightSteelBlue);
+            DynamicGrid.BorderThickness = new Windows.UI.Xaml.Thickness(2, 2, 2, 2);
+            DynamicGrid.BorderBrush = new SolidColorBrush(Colors.Black);
+
+            var height = new GridLength(1, GridUnitType.Star);
+            var width = new GridLength(1, GridUnitType.Star);
+
+            var width2 = new GridLength(2, GridUnitType.Star);
+
+            for (int i = 0; i <= 1; i++)
+            {
+                if (i == 0)
+                {
+                    DynamicGrid.ColumnDefinitions.Add(new ColumnDefinition()
+                    {
+                        Width = width
+                    });
+                }
+                if (i == 1)
+                {
+                    DynamicGrid.ColumnDefinitions.Add(new ColumnDefinition()
+                    {
+                        Width = width2
+                    });
+                }
+            }
+
+            var obj = App.Current as App;
+            string one = obj.muscleExer1;
+            int counter = 1;
+
+            string[] boxes = new string[6];
+            boxes[0] = obj.muscleExer1;
+            boxes[1] = obj.muscleExer2;
+            boxes[2] = obj.muscleExer3;
+            boxes[3] = obj.muscleExer4;
+            boxes[4] = obj.muscleExer5;
+            boxes[5] = obj.muscleExer6;
+
+            for (int i = 0; i <= 6; i++)
+            {
+
+                DynamicGrid.RowDefinitions.Add(new RowDefinition()
+                {
+                    Height = height
+                });
+
+            }
+
+            for (int i = 0; i <= 42; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    if (boxes[j] == reee[i, 1])
+                    {
+                        Border myBorder1 = new Border();
+                        myBorder1.BorderBrush = new SolidColorBrush(Colors.Black);
+                        myBorder1.BorderThickness = new Thickness(1);
+
+                        Image img = new Image();
+                        BitmapImage tmp = new BitmapImage(new Uri(reee[i, 2], UriKind.Absolute));
+                        img.Source = tmp;
+                        img.Stretch = Stretch.Fill;
+                        Grid.SetRow(img, counter);
+                        Grid.SetColumn(img, 0);
+                        DynamicGrid.Children.Add(img);
+
+                        TextBlock label1 = new TextBlock();
+                        label1.Text = reee[i, 1];
+                        label1.FontSize = 10;
+                        label1.VerticalAlignment = VerticalAlignment.Top;
+                        label1.HorizontalAlignment = HorizontalAlignment.Center;
+                        Grid.SetColumn(label1, 1);
+                        Grid.SetRow(label1, counter);
+                        DynamicGrid.Children.Add(label1);
+
+                        TextBlock label2 = new TextBlock();
+                        label2.Text = reee[i, 3];
+                        label2.FontSize = 10;
+                        label2.VerticalAlignment = VerticalAlignment.Center;
+                        Grid.SetColumn(label2, 1);
+                        Grid.SetRow(label2, counter);
+                        DynamicGrid.Children.Add(label2);
+
+                        Run run1 = new Run();
+                        run1.Text = "For a link to a YouTube video click here";
+
+                        Hyperlink hyperlink = new Hyperlink()
+                        {
+                            NavigateUri = new Uri(reee[i, 4])
+                        };
+
+                        Border myBorder3 = new Border();
+                        myBorder3.BorderBrush = new SolidColorBrush(Colors.Black);
+                        myBorder3.BorderThickness = new Thickness(1);
+
+                        TextBlock label3 = new TextBlock();
+                        label3.FontSize = 10;
+                        label3.VerticalAlignment = VerticalAlignment.Bottom;
+
+                        hyperlink.Inlines.Add(run1);
+                        label3.Inlines.Add(hyperlink);
+
+                        myBorder3.Child = label3;
+
+                        Grid.SetColumn(myBorder3, 1);
+                        Grid.SetRow(myBorder3, counter);
+                        DynamicGrid.Children.Add(myBorder3);
+
+                        counter++;
                     }
                 }
             }
